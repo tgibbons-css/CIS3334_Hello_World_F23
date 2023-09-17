a7a88c86c915fc88389648b1d57b151abcb8e492
Tom Gibbons <tgibbons@css.edu>
2023-09-16 19:08:07 -0500

    Working version of code

---
+++ app/src/main/java/cis3334/helloworldf23/MainActivity.java
@@ -0,0 +1,33 @@
+package cis3334.helloworldf23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textViewMessage;
+    Button buttonPressMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = findViewById(R.id.textViewMessage);
+        setupButton();
+    }
+
+    private void setupButton() {
+        buttonPressMe = findViewById(R.id.buttonPressMe);
+        buttonPressMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewMessage.setText("Hello World");
+            }
+        });
+    }
+}
